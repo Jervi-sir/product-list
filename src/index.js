2a158befe2a9e6208675355b0eac655e36bd9ad1
jervi <gacembekhira@gmail.com>
2022-12-07 18:53:56 +0100

    conencted firebase

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const firebaseConfig = {
+  apiKey: "AIzaSyC54IOpE6Fb9m1Of35wAhWz2a14GdkkLOY",
+  authDomain: "pructl.firebaseapp.com",
+  projectId: "pructl",
+  storageBucket: "pructl.appspot.com",
+  messagingSenderId: "98696611505",
+  appId: "1:98696611505:web:d86b8b3b64be9a9453c490",
+  measurementId: "G-QZ66ZX07RF"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 root.render(
   <React.StrictMode>
     <App />
